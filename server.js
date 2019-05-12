4234769d36b68a349f8a75e02fbcf16b3183fc3c
upstairskid <45738525+upstairskid@users.noreply.github.com>
2019-05-12 01:14:35 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,35 +1,47 @@
-// *********************************************************************
-// SERVER
-// *********************************************************************
+const express = require('express'),
+app = express(),
+passport = require('passport'),
+session = require('express-session'),
+bodyParser=require('body-parser'),
+env = require('dotenv').config({path:__dirname+'/.env'}),
+exphbs = require('express-handlebars')
 
-//REQUIRE
-//======================================================================
-require("dotenv").config();
-
-const express = require('express');
-const bodyParser = require("body-parser");
-const exphbs = require("express-handlebars");
-
-const app = express();
-const PORT = process.env.PORT || 3006;
-
-//Set up static files using express
-app.use(express.static(process.cwd() + "/public"));
-
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-//Set handlebars as engine
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.use(session({
+    secret:"neighbourhood",
+    resave:true,
+    saveUninitialized:true
+}))
+app.use(passport.initialize());
+app.use(passport.session());
 
-//Require Routes
-var routes = require("./routes/html-routes");
+app.set('views','./app/views')
+app.engine('hbs',exphbs({
+    extname:'.hbs'
+}))
+app.set('view engine','.hbs')
 
-app.use(routes);
+app.get('/',function(req,res){
+    res.render('home')
+})
 
-//TODO Force Sync
-app.listen(PORT, function() {
-    // eslint-disable-next-line no-console
-    console.log("Server listening on: http://localhost:" + PORT);
-  });
+const models = require('./app/models')
+
+const authRoute = require('./app/routes/auth.js')(app,passport)
+require('./app/config/passport/passport.js')(passport, models.user);
+
+models.sequelize.sync().then(function(){
+    console.log('Database working')
+}).catch(function(err){
+    console.log(err,"sth were wrong")
+})
+
+app.listen(3006,function(err){
+    if(!err){
+        console.log('app listening on port 3000')
+    }else{
+        console.log(err)
+    }
+})
