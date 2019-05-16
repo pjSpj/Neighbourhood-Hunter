b7cc394bfe6a0f543d9720ad824109df951df1fd
upstairskid <45738525+upstairskid@users.noreply.github.com>
2019-05-16 13:35:59 -0400

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -5,13 +5,13 @@ module.exports = function(app,passport){
     app.get('/signin', authController.signin);
     
     app.post('/signup', passport.authenticate('local-signup', {
-        successRedirect: '/favourites',
+        successRedirect: '/survey',
         failureRedirect: '/signup'
     }
     ));
 
     app.post('/signin', passport.authenticate('local-signup', {
-        successRedirect: '/favourites',
+        successRedirect: '/survey',
         failureRedirect: '/signin'
     }
     ));
